ca26a160f398c96231a8fe6df3962ef3ca957b40
Aayush7105 <aayushrawat5107@gmail.com>
2025-09-23 12:41:54 +0530

    Done with some easy questions of Strivers A2Z DSA Course.

---
+++ Array/Easy/Largest Element in an Array/Largest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Largest{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in); 
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+        int [] ar = new int[n]; 
+        System.out.println("Enter elements of array:");
+        for(int i=0;i<n;i++){
+            ar[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i: ar){
+            max = Math.max(max,i);
+        }
+        System.out.println("Maximum element = " + max);
+    }
+}
